61e3e2d72b04740dace91d0822433d28362b75bb
bboytera <118207247+bboytera@users.noreply.github.com>
2023-12-25 22:47:23 -0600

    agregamos descripción al readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # proyecto_tripleten
 sprint 4 
+
+Este es un dashboard interactivo en **app.py** , el cual realiza un histograma y un gráfico de dispersión, utilizando la data contenida en **vehicles_us.csv**
--- app.py
+++ app.py
@@ -6,13 +6,26 @@ import streamlit as st
 car_data = pd.read_csv("vehicles_us.csv") 
 st.header('Vehiculos')
 
-hist_button = st.button('Construir histograma') # crear un botón
+hist_button = st.checkbox('Construir un histograma') # crear un botón
         
 if hist_button:
         st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
             
 # crear un histograma
 fig = px.histogram(car_data, x="odometer")
+        
+# mostrar un gráfico Plotly interactivo
+st.plotly_chart(fig, use_container_width=True) 
+
+
+disper_button = st.button('Construir un grafico de dispersión') # crear un botón
+        
+if disper_button:
+        st.write('Creación de un grafico de dispersión para el conjunto de datos de anuncios de venta de coches')
+            
+# crear un grafico de dispersión
+fig = px.scatter(car_data, x="odometer", y="price") # crear un gráfico de dispersión
+
         
 # mostrar un gráfico Plotly interactivo
 st.plotly_chart(fig, use_container_width=True) 
