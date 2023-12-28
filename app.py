4762062cf640d7d889137855bb09d3a2e6128a68
bboytera <118207247+bboytera@users.noreply.github.com>
2023-12-27 18:48:18 -0600

    cambios

--- app.py
+++ app.py
@@ -9,23 +9,23 @@ st.header('Vehiculos')
 hist_button = st.checkbox('Construir un histograma') # crear un botón
         
 if hist_button:
-        st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
             
 # crear un histograma
-fig = px.histogram(car_data, x="odometer")
+    fig = px.histogram(car_data, x="odometer")
         
 # mostrar un gráfico Plotly interactivo
-st.plotly_chart(fig, use_container_width=True) 
+    st.plotly_chart(fig, use_container_width=True) 
 
 
 disper_button = st.button('Construir un grafico de dispersión') # crear un botón
         
 if disper_button:
-        st.write('Creación de un grafico de dispersión para el conjunto de datos de anuncios de venta de coches')
+    st.write('Creación de un grafico de dispersión para el conjunto de datos de anuncios de venta de coches')
             
 # crear un grafico de dispersión
-fig = px.scatter(car_data, x="odometer", y="price") # crear un gráfico de dispersión
+    fig = px.scatter(car_data, x="odometer", y="price") # crear un gráfico de dispersión
 
         
 # mostrar un gráfico Plotly interactivo
-st.plotly_chart(fig, use_container_width=True) 
+    st.plotly_chart(fig, use_container_width=True) 
